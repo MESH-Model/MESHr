3530da4a7fc76b8ddf86ae629e2b1bae48a88396
Kevin Shook <kevin.shook@usask.ca>
2018-09-23 16:59:08 -0600

    added vignette for simpleHydrograph

--- R/MESHr-package.R
+++ R/MESHr-package.R
@@ -8,7 +8,7 @@
 
 #' @references 
 #' To cite \pkg{MESHr} in publications, use the command \code{citation("MESHr")} to get the current version of the citation.\cr
-#' @import grid ggplot2 reshape2 stringr
+#' @import grid ggplot2 reshape2 stringr knitr
 #' @importFrom stats na.omit 
 #' @importFrom utils read.table
 NULL
