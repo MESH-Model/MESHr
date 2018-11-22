6b391b6525b0ca10cd66c69432c45a0f111b4d08
Kevin Shook <kevin.shook@usask.ca>
2018-11-22 14:23:52 -0600

    fixed major bugs in distributePrecip

--- R/distribPrecip.R
+++ R/distribPrecip.R
@@ -6,15 +6,23 @@
 #' (e.g. daily) according to a  set of high frequency precipitation 
 #' (e.g. hourly). The high-frequency values are summed to have the same time 
 #' intervals as the low-frequency data. The ratios of low/high freqency 
-#' precipiations are determined for each time step, and these ratios
-#' are multiplied by the high-frequency data
+#' precipitations are determined for each time step, and these ratios
+#' are multiplied by the high-frequency data. Where the high-frequency total
+#' precipitation is zero, the low-frequency data is spread evenly over the 
+#' high-frequency interval.
 #' @param LFprecip Required. A data frame of low temporal frequency (e.g. daily)
 #' precipitation. The first column must be a POSIXct date/time called 
 #' \code{datetime}. The second column must be the precipitation.
 #' @param HFprecip Required. A data frame of high temporal frequency 
 #' (e.g. hourly) precipitation. The first column must be a POSIXct date/time 
 #' called \code{datetime}. The second column must be the precipitation.
-#'
+#' @param zero_missing_HF Optional. If \code{TRUE} (the default) 
+#' missing high-frequency precipitation values are replaced with zeros.
+#' @param period_thresholds Length of maximum infilled period in hours. 
+#' Sequences of missing low-frequency values exceeding this length will be set
+#' to \code{NA_real_}, and the next low-frequency value will be assigned to
+#' the next high frequency interval. This is usually required when a gauge 
+#' is not reported for a seasonal period.
 #' @return Returns a data frame of the adjusted high-frequency precipitation, 
 #' with the variables \code{datetime} and \code{distributedP}. 
 #' Note that the last date/time in the returned data corresponds to the final
@@ -26,7 +34,9 @@
 #' @examples \dontrun{distributed <- distribPrecip(myPrecip[, c(1, 5)], 
 #' adjacentP)
 #' }
-distribPrecip <- function(LFprecip = NULL, HFprecip = NULL) {
+distribPrecip <- function(LFprecip = NULL, HFprecip = NULL, 
+                          zero_missing_HF = TRUE,
+                          period_threshold = 48) {
   
   if (is.null(LFprecip)) {
     cat("Error: missing Low Frequency file\n")
@@ -42,6 +52,10 @@ distribPrecip <- function(LFprecip = NULL, HFprecip = NULL) {
   merged <- merge(HFprecip, LFprecip, by = "datetime", all.x = TRUE)
   names(merged) <- c("datetime", "HF", "LF")
   
+  # set missing HF data to zero if requested
+  if (zero_missing_HF)
+    merged$HF[is.na(merged$HF)] <- 0
+
   # find end of LF data and terminate data frame
   merged$rownums <- seq(1:nrow(merged))
   max_LF_row <- max(merged$rownums[!is.na(merged$LF)])
@@ -50,6 +64,10 @@ distribPrecip <- function(LFprecip = NULL, HFprecip = NULL) {
   test <- !is.na(merged$LF) > 0
   y <- cumsum(test)
   merged$periods <- c(y[1], y[-length(y)]) + 1
+  runs <- rle(merged$periods)
+  run_lengths <- runs$lengths
+  run_values <- runs$values
+  i <- inverse.rle(runs)
   
   # get precip sum in each run
   period_sums <- aggregate(merged$HF, by = list(merged$periods), FUN = "sum")
@@ -58,12 +76,29 @@ distribPrecip <- function(LFprecip = NULL, HFprecip = NULL) {
   # get HF/LF ratios for each period
   LF <- merged$LF[!is.na(merged$LF)]
 
-
+  period_sums$LF <- LF
   period_sums$ratio <- LF / period_sums$HF_sum
-  period_sums$ratio[is.infinite(period_sums$ratio)] <- 0
+  period_sums$length <- run_lengths
+  
+  # distribute precip evenly over the interval if there is no HF precip
+  period_sums$even_dist <- NA_real_
+  infinite_locs <- which(is.infinite(period_sums$ratio))
+  period_sums$even_dist[infinite_locs] <- period_sums$LF[infinite_locs] /
+    period_sums$length[infinite_locs]
+  
   merged$ratio <- period_sums$ratio[merged$periods]
   merged$ratio[is.nan(merged$ratio)] <- 0
   merged$distributedP <- merged$HF * merged$ratio
+  merged$period_length <- period_sums$length[merged$periods]
+  
+  merged$even_dist <- period_sums$even_dist[merged$periods]
+  merged$distributedP[is.infinite(merged$ratio)] <- 
+    merged$even_dist[is.infinite(merged$ratio)] 
+  
+  # set all but last merged value to NA, when run length > threshold
+  merged$distributedP[merged$period_length > period_threshold] <- NA_real_
+  last_p_locs <- (merged$period_length > period_threshold) & !is.na(merged$LF) 
+  merged$distributedP[last_p_locs] <- merged$LF[last_p_locs]
   
   # return distributed values
   distributed <- merged[,c("datetime", "distributedP")]
---
+++ R/read_MESH_binary_grid.R
@@ -0,0 +1,53 @@
+read_MESH_binary_grid <- function(binaryFile, shedFile) {
+  
+  # get watershed grid definition
+  shed <- read_r2c_shed(shedFile, as_rasters = FALSE)
+  shed_locs <- shed[,,1]    # get basin point locations
+  
+  # get row and cols of shed_locs
+  shed_rows_cols <- which(shed_locs > 0, arr.ind = TRUE)
+  shed_rows_cols <- as.matrix(shed_rows_cols)
+  
+  # add values
+  shed_rows_cols <- cbind(shed_rows_cols, shed_locs[shed_rows_cols])
+  names(shed_rows_cols) <- c( "row", "col", "loc")
+  num_vals <- nrow(shed_rows_cols)
+  
+  # order by value
+  shed_rows_cols <- shed_rows_cols[order(shed_rows_cols$loc),]
+  
+  # now read in binary file
+  binary <- file(binaryFile, "rb")
+  year <- c(0)
+  month <- c(0)
+  day <- c(0)
+  hour <- c(0)
+  min <- c(0)
+  all_frame_vals <- matrix[]
+  done <- FALSE
+  while (!done) {
+    header <- readBin(binary, integer(), 9)
+    if (length(header) == 0) {
+      record_num <- header[3]
+      year[record_num] <- header[4]
+      month[record_num] <- header[5]
+      day[record_num] <- header[6]
+      hour[record_num] <- header[7]
+      min[record_num] <- header[8]
+      
+      record_length <-  readBin(binary, integer(), 1)
+      frame_vals <- readBin(binary, numeric(), n = num_vals, size = 4)
+      record_length <-  readBin(binary, integer(), 1)
+      frame_matrix <- data.matrix(frame_vals)
+      all_frame_vals[,,record_num] <- frame_matrix
+    } else {
+      done <- TRUE
+    }
+  }
+
+  
+  close(binaryFile)
+  
+  return(all_frame_vals)
+  
+}
